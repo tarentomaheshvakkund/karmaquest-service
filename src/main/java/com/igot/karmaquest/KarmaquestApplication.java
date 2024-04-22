e388d09240c1e12cd4cd852a545336347d010fdf
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2024-04-22 13:10:15 +0530

    -Initial Commit.

---
+++ src/main/java/com/igot/karmaquest/KarmaquestApplication.java
@@ -0,0 +1,13 @@
+package com.igot.karmaquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmaquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmaquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igot/karmaquest/KarmaquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igot.karmaquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmaquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
